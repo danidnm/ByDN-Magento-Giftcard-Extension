0a505cb60c9508a46aaf3c56a64d16ba8d13bbe9
Daniel Navarro <danidnm@gmail.com>
2024-10-17 10:15:23 +0200

    Code review

--- Helper/Config.php
+++ Helper/Config.php
@@ -4,23 +4,25 @@ namespace Bydn\Giftcard\Helper;
 
 class Config extends \Magento\Framework\App\Helper\AbstractHelper
 {
-    const PATH_GIFTCARD_ENABLE = 'bydn_giftcard/general/enable';
-    const PATH_GIFTCARD_AMOUNTS = 'bydn_giftcard/general/amounts';
-    const PATH_GIFTCARD_EMAIL_ENABLED = 'bydn_giftcard/emails/send_by_email';
-    const PATH_GIFTCARD_EMAIL_SENDER = 'bydn_giftcard/emails/sender_email_identity';
-    const PATH_GIFTCARD_EMAIL_TEMPLATE = 'bydn_giftcard/emails/email_template';
-    const PATH_GIFTCARD_SINGLE_USE_ENABLED = 'bydn_giftcard/expiration/single_use';
-    const PATH_GIFTCARD_EXPIRATION_ENABLED = 'bydn_giftcard/expiration/expire_cards';
-    const PATH_GIFTCARD_EXPIRATION_TIME = 'bydn_giftcard/expiration/expiration_time';
-    const PATH_GIFTCARD_WITH_GIFTCARD = 'bydn_giftcard/discounts/avoid_giftcard_with_giftcard';
-    const PATH_GIFTCARD_AVOID_DISCOUNTS = 'bydn_giftcard/discounts/avoid_discounts_on_giftcards';
+    private const PATH_GIFTCARD_ENABLE = 'bydn_giftcard/general/enable';
+    private const PATH_GIFTCARD_AMOUNTS = 'bydn_giftcard/general/amounts';
+    private const PATH_GIFTCARD_EMAIL_ENABLED = 'bydn_giftcard/emails/send_by_email';
+    private const PATH_GIFTCARD_EMAIL_SENDER = 'bydn_giftcard/emails/sender_email_identity';
+    private const PATH_GIFTCARD_EMAIL_TEMPLATE = 'bydn_giftcard/emails/email_template';
+    private const PATH_GIFTCARD_SINGLE_USE_ENABLED = 'bydn_giftcard/expiration/single_use';
+    private const PATH_GIFTCARD_EXPIRATION_ENABLED = 'bydn_giftcard/expiration/expire_cards';
+    private const PATH_GIFTCARD_EXPIRATION_TIME = 'bydn_giftcard/expiration/expiration_time';
+    private const PATH_GIFTCARD_WITH_GIFTCARD = 'bydn_giftcard/discounts/avoid_giftcard_with_giftcard';
+    private const PATH_GIFTCARD_AVOID_DISCOUNTS = 'bydn_giftcard/discounts/avoid_discounts_on_giftcards';
 
     /**
      * Get default giftcard amounts
-     * @param $store_id
+     *
+     * @param int $store_id
      * @return mixed
      */
-    public function isEnabled($store_id = null) {
+    public function isEnabled($store_id = null)
+    {
         return $this->scopeConfig->getValue(
             self::PATH_GIFTCARD_ENABLE,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
@@ -30,10 +32,12 @@ class Config extends \Magento\Framework\App\Helper\AbstractHelper
 
     /**
      * Get default giftcard amounts
-     * @param $store_id
+     *
+     * @param int $store_id
      * @return mixed
      */
-    public function getDefaultAmounts($store_id = null) {
+    public function getDefaultAmounts($store_id = null)
+    {
         return $this->scopeConfig->getValue(
             self::PATH_GIFTCARD_AMOUNTS,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
@@ -43,10 +47,12 @@ class Config extends \Magento\Framework\App\Helper\AbstractHelper
 
     /**
      * Check if sending emails is enabled
-     * @param $store_id
+     *
+     * @param int $store_id
      * @return mixed
      */
-    public function isEmailEnabled($store_id = null) {
+    public function isEmailEnabled($store_id = null)
+    {
         return $this->scopeConfig->getValue(
             self::PATH_GIFTCARD_EMAIL_ENABLED,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
@@ -56,10 +62,12 @@ class Config extends \Magento\Framework\App\Helper\AbstractHelper
 
     /**
      * Returns the email sender identity
-     * @param $store_id
+     *
+     * @param int $store_id
      * @return mixed
      */
-    public function getEmailSender($store_id = null) {
+    public function getEmailSender($store_id = null)
+    {
         return $this->scopeConfig->getValue(
             self::PATH_GIFTCARD_EMAIL_SENDER,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
@@ -69,10 +77,12 @@ class Config extends \Magento\Framework\App\Helper\AbstractHelper
 
     /**
      * Returns the email template to be used
-     * @param $store_id
+     *
+     * @param int $store_id
      * @return mixed
      */
-    public function getEmailTemplate($store_id = null) {
+    public function getEmailTemplate($store_id = null)
+    {
         return $this->scopeConfig->getValue(
             self::PATH_GIFTCARD_EMAIL_TEMPLATE,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
@@ -82,10 +92,12 @@ class Config extends \Magento\Framework\App\Helper\AbstractHelper
 
     /**
      * Checks if card expiration is enabled
-     * @param $store_id
+     *
+     * @param int $store_id
      * @return mixed
      */
-    public function isSingleUseEnabled($store_id = null) {
+    public function isSingleUseEnabled($store_id = null)
+    {
         return $this->scopeConfig->getValue(
             self::PATH_GIFTCARD_SINGLE_USE_ENABLED,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
@@ -95,10 +107,12 @@ class Config extends \Magento\Framework\App\Helper\AbstractHelper
 
     /**
      * Checks if card expiration is enabled
-     * @param $store_id
+     *
+     * @param int $store_id
      * @return mixed
      */
-    public function isExpirationEnabled($store_id = null) {
+    public function isExpirationEnabled($store_id = null)
+    {
         return $this->scopeConfig->getValue(
             self::PATH_GIFTCARD_EXPIRATION_ENABLED,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
@@ -108,10 +122,12 @@ class Config extends \Magento\Framework\App\Helper\AbstractHelper
 
     /**
      * Checks if card expiration is enabled
-     * @param $store_id
+     *
+     * @param int $store_id
      * @return mixed
      */
-    public function getExpirationDays($store_id = null) {
+    public function getExpirationDays($store_id = null)
+    {
         return $this->scopeConfig->getValue(
             self::PATH_GIFTCARD_EXPIRATION_TIME,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
@@ -121,10 +137,12 @@ class Config extends \Magento\Framework\App\Helper\AbstractHelper
 
     /**
      * Check if we should avoid paying a giftcard with another giftcard
-     * @param $store_id
+     *
+     * @param int $store_id
      * @return mixed
      */
-    public function avoidGiftcardWithGiftcard($store_id = null) {
+    public function avoidGiftcardWithGiftcard($store_id = null)
+    {
         return $this->scopeConfig->getValue(
             self::PATH_GIFTCARD_WITH_GIFTCARD,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
@@ -134,10 +152,12 @@ class Config extends \Magento\Framework\App\Helper\AbstractHelper
 
     /**
      * Checks if we should avoid any discount applied to a giftcard
-     * @param $store_id
+     *
+     * @param int $store_id
      * @return mixed
      */
-    public function avoidAnyDiscount($store_id = null) {
+    public function avoidAnyDiscount($store_id = null)
+    {
         return $this->scopeConfig->getValue(
             self::PATH_GIFTCARD_AVOID_DISCOUNTS,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
