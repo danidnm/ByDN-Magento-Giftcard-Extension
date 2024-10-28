6aaed9af96e76800d4e52517bc2c46e69df924d8
Daniel Navarro <danidnm@gmail.com>
2024-10-28 16:32:05 +0100

    Code review

--- Model/Sales/Pdf/Giftcard.php
+++ Model/Sales/Pdf/Giftcard.php
@@ -8,47 +8,9 @@ use Magento\Tax\Model\ResourceModel\Sales\Order\Tax\CollectionFactory;
 
 class Giftcard extends \Magento\Sales\Model\Order\Pdf\Total\DefaultTotal
 {
-    public function __construct(
-        Data $taxHelper,
-        Calculation $taxCalculation,
-        CollectionFactory $ordersFactory,
-        array $data = []
-    ) {
-        parent::__construct($taxHelper, $taxCalculation, $ordersFactory, $data);
-    }
-
     /**
-     * Get array of arrays with totals information for display in PDF
-     * array(
-     *  $index => array(
-     *      'amount'   => $amount,
-     *      'label'    => $label,
-     *      'font_size'=> $font_size
-     *  )
-     * )
-     * @return array
-     */
-//    public function getTotalsForDisplay(): array
-//    {
-//        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-//        $partialpayment = $objectManager->create('\Vendor\Module\Model\Partialpayment');
-//        $partialPayment = $partialpayment->load($this->getOrder()->getIncrementId(),'order_id');
-//        $paynow = $partialPayment->getPaidAmount();
-//        if (!$partialPayment->getOrderId()) {
-//            return [];
-//        }
-//        $amountInclTax = $this->getOrder()->formatPriceTxt($paynow);
-//        $fontSize = $this->getFontSize() ? $this->getFontSize() : 7;
-//        return [
-//            [
-//                'amount' => 1, //$this->getAmountPrefix() . $amountInclTax,
-//                'label' => 'Giftcard', //__($this->getTitle()) . ':',
-//                'font_size' => 12 // $fontSize,
-//            ]
-//        ];
-//    }
-
-    /**
+     * Returns giftcard applied amount
+     *
      * @return float|int|void
      */
     public function getAmount()
